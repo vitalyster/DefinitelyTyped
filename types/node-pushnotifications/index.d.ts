89b1a1d1f1888ce9d9c3d595a43d82587caf67df
vitalyster <vitalyster@gmail.com>
2021-11-13 12:30:53 +0300

    title and body are optional

--- types/node-pushnotifications/index.d.ts
+++ types/node-pushnotifications/index.d.ts
@@ -105,10 +105,8 @@ declare namespace PushNotifications {
         isAlwaysUseFCM?: boolean | undefined;
     }
     interface Data {
-        /** REQUIRED */
-        title: string;
-        /** REQUIRED */
-        body: string;
+        title?: string | undefined;
+        body?: string | undefined;
         custom?: { [key: string]: string | number } | string | undefined;
         /**
          * gcm, apn. Supported values are 'high' or 'normal' (gcm). Will be translated to 10 and 5 for apn. Defaults
